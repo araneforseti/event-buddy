50852b02caaad941c2babf0d2c6abc8727487b4d
Sarah Hutchins <974016+araneforseti@users.noreply.github.com>
2022-11-13 14:15:48 +0100

    Navigation to details sends event name

--- app/src/main/java/com/semblanceoffunctionality/eventbuddy/ui/event/EventFragment.kt
+++ app/src/main/java/com/semblanceoffunctionality/eventbuddy/ui/event/EventFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.semblanceoffunctionality.eventbuddy.R
 import com.semblanceoffunctionality.eventbuddy.databinding.FragmentEventBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,21 +16,21 @@ import dagger.hilt.android.AndroidEntryPoint
 class EventFragment : Fragment() {
 
     private lateinit var binding: FragmentEventBinding
-
-    private val viewEventModel: EventViewModel by viewModels()
+    val args: EventFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        val eventName = args.event
         binding = DataBindingUtil.inflate<FragmentEventBinding>(
             inflater,
             R.layout.fragment_event,
             container,
             false
         ).apply {
-            viewModel = viewEventModel
+            viewModel = EventViewModel(eventName)
             println("------")
             println(viewModel?.name)
             println("------")
--- app/src/main/java/com/semblanceoffunctionality/eventbuddy/ui/event/EventViewModel.kt
+++ app/src/main/java/com/semblanceoffunctionality/eventbuddy/ui/event/EventViewModel.kt
@@ -3,13 +3,11 @@ package com.semblanceoffunctionality.eventbuddy.ui.event
 import androidx.lifecycle.ViewModel
 import com.semblanceoffunctionality.eventbuddy.data.Event
 
-class EventViewModel() : ViewModel() {
-    //var name: String = savedStateHandle.get<String>(EVENT_NAME_SAVED_STATE_KEY)!!
-    val event = Event("Sample Event", "01/01/2023", "01/12/2023")
+class EventViewModel(eventName: String) : ViewModel() {
+    val event = Event(eventName, "01/01/2023", "01/12/2023")
 
     val name
-        //get() = event.name
-        get() = "Sample Event"
+        get() = event.name
     val start
         //get() = event.dateStart
         get() = "01/01/2023"
