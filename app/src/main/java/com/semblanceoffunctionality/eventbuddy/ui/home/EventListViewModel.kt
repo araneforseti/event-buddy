03893a1138290e3b83fdfce3989cd78d2c70f272
Sarah Hutchins <974016+araneforseti@users.noreply.github.com>
2022-11-13 11:34:27 +0100

    List UI essentially done

--- app/src/main/java/com/semblanceoffunctionality/eventbuddy/ui/home/EventListViewModel.kt
+++ app/src/main/java/com/semblanceoffunctionality/eventbuddy/ui/home/EventListViewModel.kt
@@ -6,9 +6,9 @@ import com.semblanceoffunctionality.eventbuddy.data.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 
 class EventListViewModel : ViewModel() {
-    private val buda : Event = Event("Budafest")
-    private val king : Event = Event("King Swing")
-    private val tlv : Event = Event("TLV")
+    private val buda : Event = Event("Budafest", "11/01/23", "13/01/23")
+    private val king : Event = Event("King Swing", "17/03/23", "21/03/23")
+    private val tlv : Event = Event("TLV", "11/05/23", "14/05/23")
     private val list = arrayListOf(buda, king, tlv)
     val items = MutableLiveData<List<Event>>().apply {
         value = list
--- app/src/main/java/com/semblanceoffunctionality/eventbuddy/ui/home/EventViewModel.kt
+++ app/src/main/java/com/semblanceoffunctionality/eventbuddy/ui/home/EventViewModel.kt
@@ -5,4 +5,8 @@ import com.semblanceoffunctionality.eventbuddy.data.Event
 class EventViewModel(val event: Event) {
     val name
         get() = event.name
+    val start
+        get() = event.dateStart
+    val end
+        get() = event.dateEnd
 }
